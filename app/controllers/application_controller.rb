08acba0b66fffa6f55ef8dfd017979e7e436e6a6
Rasarts <rasart.pro@gmail.com>
2013-03-19 23:38:59 +0400

    Add_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+private
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  helper_method :current_user
+  
+  def auth
+    redirect_to login_url, alert: "Not a Admin" if current_user.nil?
+  end
+  
 end
