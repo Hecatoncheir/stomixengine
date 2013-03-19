a3f90e9906c491a86d97510a464544a29d850dba
Rasarts <rasart.pro@gmail.com>
2013-03-19 21:32:58 +0400

    Up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StomixEngine::Application.config.secret_token = 'fdb7211060a019c04d1d91cea37526e4f92e43e822cd59b6155f6ae68df1e3db9e46e6c77da161c54dc54ba37f0e273f0ee67ddb40572ab52b88b82146e26c6c'
