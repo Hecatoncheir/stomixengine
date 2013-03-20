746244b32624ca35af3e499139257e9844ec723c
Rasarts <rasart.pro@gmail.com>
2013-03-21 01:26:41 +0400

    Add title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,17 @@
 module ApplicationHelper
   
   def page_title(title)
-    content_for :page_title, title.to_s
+      @page_title = title.to_s
   end
+  
+  def title
+    default_title = "Stomix"
+    if @page_title.nil?
+      default_title
+    else
+      "#{@page_title} |  #{default_title}"
+    end
+  end
+  
   
 end
