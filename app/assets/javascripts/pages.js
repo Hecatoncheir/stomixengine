03ed56c6ceb84c32795e8908102840cceb707b25
Rasarts <rasart.pro@gmail.com>
2013-04-10 01:25:24 +0400

    Адаптация шаблона

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,2 +1,9 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+	$(".fb").fancybox({
+			openEffect: 'elastic',
+		    openSpeed: 200,
+		    closeEffect: 'elastic',
+		    closeSpeed: 200,
+			'overlayShow'	:	false
+		});
+	});
